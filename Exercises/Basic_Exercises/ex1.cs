a24f7563bf5f85d841cb850cf1a967cd35115655
Linus <linus-boehm@web.de>
2025-07-03 17:12:41 +0545

    update

--- Exercises/Basic_Exercises/ex1.cs
+++ Exercises/Basic_Exercises/ex1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 
 //Write a C# Sharp program to print Hello and your name in a separate line. 
 class Program1{
@@ -38,7 +39,8 @@ class Program4{
 
 
 //Write a C# Sharp program to swap two numbers.
-class Program5{
+class Program5
+{
     public static void Run5()
     {
         int first_num = 5;
@@ -54,13 +56,121 @@ class Program5{
     }
 }
 
-//main
-class Program{
-    public static void Main()
+
+//Write a C# Sharp program to print the output of the multiplication of three numbers entered by the user. 
+class Program6
+{
+    public static void Run6()
     {
-        Program5.Run5();
+        int num1, num2, num3;
+
+        Console.WriteLine("Multiplication 3 integer numbers:");
+        Console.Write("\nInput the first number:");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Input the second number:");
+        num2 = int.Parse(Console.ReadLine());
+        Console.Write("Input the third number:");
+        num3 = int.Parse(Console.ReadLine());
+
+        int mult_num = num1 * num2 * num3;
+
+        Console.WriteLine("Multiplication Result: " + mult_num);
+
     }
 }
+
+
+//Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing two numbers entered by the user. 
+class Program7
+{
+    public static void Run7()
+    {
+        Console.WriteLine("Input 2 interger numbers for mathematicaloperations: ");
+
+        Console.WriteLine("Number 1: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Number 2: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int add = num1 + num2;
+        int minus = num1 - num2;
+        int mult = num1 * num2;
+        int div = num1 / num2;
+
+        Console.WriteLine("Addition:" + add);
+        Console.WriteLine("Subtraction:" + minus);
+        Console.WriteLine("Multiplication:" + mult);
+        Console.WriteLine("Division:" + div);
+    }
+}
+
+
+//Write a C# Sharp program that prints the multiplication table of a number as input.
+class Program8
+{
+    public static void Run8()
+    {
+        int num_multtable;
+
+        Console.WriteLine("Input Number for Multiplication table:");
+        num_multtable = int.Parse(Console.ReadLine());
+
+        for (int a = 0; a <= 10; a++)
+        {
+            int mult_res = a * num_multtable;
+            Console.WriteLine($"{a} * {num_multtable} = " + mult_res);
+        }
+    }
+}
+
+//Write a C# Sharp program that takes four numbers as input to calculate and print the average.
+class Program9
+{
+    public static void Run9()
+    {
+        int num1, num2, num3, num4;
+
+        Console.WriteLine("Input 4 integers to calculate the average");
+
+        Console.WriteLine("Number 1:");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Number 2:");
+        num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Number 3:");
+        num3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Number 4:");
+        num4 = int.Parse(Console.ReadLine());
+
+        int average = (num1 + num2 + num3 + num4) / 4;
+
+        Console.WriteLine("Average:" + average);
+    }
+}
+
+//Write a C# Sharp program that takes three numbers (x,y,z) as input and outputs (x+y).z and x.y + y.z.
+class Program10
+{
+    public static void Run10()
+    {
+        int num1, num2, num3;
+
+        Console.WriteLine("Input 3 intergers for calculations:");
+
+        Console.WriteLine("\nInput number 1:");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Input number 2:");
+        num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Input number 3:");
+        num3 = int.Parse(Console.ReadLine());
+
+        int calc1 = (num1 + num2) * num3;
+        int calc2 = num1 * num2 + num2 * num3;
+
+        Console.WriteLine($"({num1} + {num2}) * {num3} = " + calc1);
+        Console.WriteLine($"{num1} * {num2} + {num2} * {num3} = "+  calc2);
+    }
+} 
+
 
 
 
--- Exercises/Basic_Exercises/ex2.cs
+++ Exercises/Basic_Exercises/ex2.cs
@@ -1,1 +1,101 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
+//Write a C# Sharp program that takes an age as input and prints something like "You look older than 20". 
+class Program11
+{
+    public static void Run11()
+    {
+        int age;
+
+        Console.WriteLine("Enter your age!");
+        age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("You don't look like " + age);
+    }
+}
+
+//Write a C# program that takes a number as input and displays it four times in a row (separated by blank spaces), and then four times in the next row, with no separation
+class Program12
+{
+    public static void Run12()
+    {
+        int num;
+
+        Console.WriteLine("Enter a integer to display it 4 times");
+        num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{num} {num} {num} {num}");
+        Console.WriteLine($"{num}{num}{num}{num}");
+    }
+}
+
+//Write a C# program that takes a number as input and displays a rectangle of 3 columns wide and 5 rows tall using that digit
+class Program13
+{
+    public static void Run13()
+    {
+        int num;
+
+        Console.WriteLine("Enter number for Rectangel representation:");
+        num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{num}{num}{num}");
+        Console.WriteLine($"{num} {num}");
+        Console.WriteLine($"{num} {num}");
+        Console.WriteLine($"{num} {num}");
+        Console.WriteLine($"{num}{num}{num}");
+
+
+    }
+}
+//Write a C# program to convert Celsius degrees to Kelvin and Fahrenheit.
+class Program14
+{
+    public static void Run14()
+    {
+        double cels;
+
+        Console.WriteLine("Enter Celcius degree");
+        cels = int.Parse(Console.ReadLine());
+
+        double kelv = cels + 273.15;
+        double fahr = 32 + 1.8 * cels;
+
+        Console.WriteLine($"{cels} degree Celsius match {kelv} degree Kelvin");
+        Console.WriteLine($"{cels} degree Celsius match {fahr} degree Fahrenheit");
+    }
+}
+
+
+//Write a program that removes a specified character from a non-empty string using the index of a character.
+class Program15
+{
+    public static void Run15()
+    {
+        Console.WriteLine("Enter a word:");
+        string word = Console.ReadLine();
+
+        int length = word.Length;
+
+        Console.WriteLine("Remove the ith character:");
+        int index = int.Parse(Console.ReadLine());
+
+        string word_left = word.Substring(0, index);
+        string word_right = word.Substring(index + 1, length);
+
+        string new_word = word_left + word_right;
+
+        Console.WriteLine("New word, without ith character");
+
+    }
+} 
+
+//main
+class Program
+{
+    public static void Main()
+    {
+        Program15.Run15();
+    }
+}
